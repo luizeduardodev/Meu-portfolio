cc71a917a08e361c05b379cddc3c097de3b1085d
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-04-30 22:21:50 -0300

    Refatoração e melhoria

--- assets/js/effectNavbar.js
+++ assets/js/effectNavbar.js
@@ -1,17 +1,29 @@
-/* Função para alterar o menu fixo */
+/* Função que adiciona o efeito de padding no menu */
 window.addEventListener("scroll", () => {
     const navbar = document.getElementById("navbar");
     const menu = document.getElementById("menu");
 
     if (scrollY >= 40) {
-        navbar.style.padding = "10px";
-        navbar.style.backgroundColor = "#000746";
-        navbar.style.borderBottom = "1px solid black";
-        menu.style.top = "52px";
+        navbar.classList.add("active");
+        menu.classList.add("active");
     } else {
-        navbar.style.padding = "15px";
-        navbar.style.backgroundColor = "transparent";
-        navbar.style.borderBottom = "none";
-        menu.style.top = "62px";
+        navbar.classList.remove("active");
+        menu.classList.remove("active");
     }
 });
+
+// window.addEventListener("scroll", () => {
+//      const navbar = document.getElementById("navbar");
+//      const menu = document.getElementById("menu");
+//      if (scrollY >= 40) {
+//          navbar.style.padding = "10px";
+//          navbar.style.backgroundColor = "#000746";
+//          navbar.style.borderBottom = "1px solid black";
+//          menu.style.top = "52px";
+//      } else {
+//          navbar.style.padding = "15px";
+//          navbar.style.backgroundColor = "transparent";
+//          navbar.style.borderBottom = "none";
+//          menu.style.top = "62px";
+//      }
+//});
